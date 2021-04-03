85d51d1f7c3f4471819b4d202bc5ab9220896d4e
bestb4 <67499665+bestb4@users.noreply.github.com>
2021-04-03 14:05:29 +0900

    Variables test

--- src/project/mainclass.java
+++ src/project/mainclass.java
@@ -1,7 +1,21 @@
 package project;
 
 public class mainclass {
-public static void main(String[] args) {
-	System.out.println("Hello Java World!!!!!!");
-}
-}
+
+
+	char c ='a';
+	system.out.println("c="+c);
+	
+	int i = 10;
+	system.out.println("i="+i);
+	
+	double d =10.258;
+	system.out.println("d="+d);
+	
+	boolean b = false;
+	system.out.println("b="+b);
+	
+	String s = "Hello Java World!!";
+	system.out.println("s="+s);
+	
+} 
