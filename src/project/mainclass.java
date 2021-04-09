81f5cab2ae523d1f99160394da467179a8b16c84
bestb4 <67499665+bestb4@users.noreply.github.com>
2021-04-09 15:55:14 +0900

    main

--- src/project/mainclass.java
+++ src/project/mainclass.java
@@ -2,20 +2,58 @@ package project;
 
 public class mainclass {
 
+	//복합대입 연산자
+		x=10;
+		System.out.println("x += 10:"+(x+=10));
+		x=10;
+		System.out.println("x -= 10:"+(x-=10));
+		x=10;
+		System.out.println("x *= 10:"+(x*=10));
+		x=10;
+		System.out.println("x /= 10:"+(x/=10));
+		x=10;
+		System.out.println("x %= 10:"+(x%=10));
+		
+		//관계 연산자
+		x=10; y=20;
+		System.out.println("x>y:"+(x>y));
+		System.out.println("x<y:"+(x<y));
+		System.out.println("x>=y:"+(x>=y));
+		System.out.println("x<=y:"+(x<=y));
+		System.out.println("x==y:"+(x==y));
+		System.out.println("x!=y:"+(x!=y));
+		
+	//증감연산자
+	x=10;
+	System.out.println("++x:"+(++x));
+	x=10;
+	System.out.println("--x:"+(--x));
+	x=10;
+	System.out.println("x++:"+(x++));
+	System.out.println("x:"+x);
+	x=10;
+	System.out.println("x--:"+(x--));
+	System.out.println("x:"+x);
 
-	char c ='a';
-	system.out.println("c="+c);
-	
-	int i = 10;
-	system.out.println("i="+i);
-	
-	double d =10.258;
-	system.out.println("d="+d);
-	
-	boolean b = false;
-	system.out.println("b="+b);
-	
-	String s = "Hello Java World!!";
-	system.out.println("s="+s);
-	
+
+	//논리 연산자
+	boolean b1 = false;
+	boolean b2 = true;
+	System.out.println("b1&&b2:"+(b1 && b2));
+	System.out.println("b1||b2:"+(b1||b2));
+	System.out.println("!b1:"+!b1);
+	System.out.println("!b2:"+!b2);
+
+
+	//조건(삼항)연산자
+	x=10;y=20;
+	result = (x>y) ? 100 : 200;
+	System.out.println("result:"+ result);
+
+	result = (x<y) ? 100 : 200;
+	System.out.println("result:"+ result);
+
+	result = (x==y) ? 100:200;
+	System.out.println("result:"+ result);
+
 } 
